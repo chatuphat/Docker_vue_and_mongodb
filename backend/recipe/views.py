44d6fb393c7e141aa86c544ee0d8e7e3b2d419f1
chatuphat laosomboon <63543816+chatuphat@users.noreply.github.com>
2023-03-02 10:33:26 +0700

    Add django api

---
+++ backend/recipe/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .serializers import RecipeSerializer #impor the serializer we just created
+from .models import Recipe
+
+class recipe_view_set(viewsets.ModelViewSet):
+    # define queryset
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
